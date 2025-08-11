8f9519545330c726ba2d4ee86fceef889a20a73e
Amara bibi <amararashid8888@gmail.com>
2025-08-11 16:24:18 +0500

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,9 @@
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3 fixed-top">
+    <div class="container-fluid">
+        <span class="navbar-brand mx-auto fw-bold">SPS-BMS</span>
+        <div class="d-flex align-items-center">
+            <span class="text-white me-2">Welcome Nsma</span>
+            <img src="https://cdn-icons-png.flaticon.com/512/847/847969.png" alt="User" class="rounded-circle bg-white" width="30" height="30">
+        </div>
+    </div>
+</nav>

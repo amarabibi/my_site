62f5dfaa30f416b767004bd31d990eb216864d29
Amara bibi <amararashid8888@gmail.com>
2025-08-11 16:24:37 +0500

    Add files via upload

---
+++ sidebar.php
@@ -0,0 +1,13 @@
+  <div class="sidebar bg-dark text-white">
+    <a href="#"><i class="fas fa-home"></i> <span>Home</span></a>
+    <a href="#"><i class="fas fa-users"></i> <span>HR</span></a>
+    <a href="#"><i class="fas fa-file-invoice-dollar"></i> <span>Accounting</span></a>
+    <a href="#"><i class="fas fa-balance-scale"></i> <span>Legal</span></a>
+    <a href="#"><i class="fas fa-desktop"></i> <span>IT</span></a>
+    <a href="#"><i class="fas fa-concierge-bell"></i> <span>Services</span></a>
+    <a href="#"><i class="fas fa-graduation-cap"></i> <span>Education</span></a>
+    <a href="#"><i class="fas fa-chart-line"></i> <span>Sales</span></a>
+    <a href="#"><i class="fas fa-bullhorn"></i> <span>Marketing</span></a>
+    <a href="#"><i class="fas fa-tasks"></i> <span>Spintasks</span></a>
+    <a href="#"><i class="fas fa-briefcase"></i> <span>Jobcode</span></a>
+</div>
